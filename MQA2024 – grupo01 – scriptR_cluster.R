92243f27fb85831b89278a870b4a570bc5319e3e
Joao Pedro Marcondes <joaopopm@usp.br>
2024-11-12 16:57:27 -0300

    Adicionei arquivo de Clusterizacao

---
+++ MQA2024 – grupo01 – scriptR_cluster.R
@@ -0,0 +1,79 @@
+####Instalacao das dependencias
+if (!requireNamespace("pacman", quietly = TRUE)) {
+  install.packages("pacman")
+}
+
+library(pacman)
+pacman::p_load(dplyr, ggplot2, readxl, readr, DT, fastDummies)
+
+# Definir funções importantes
+moda <- function(x) {
+  modal <- unique(x)
+  modal[which.max(tabulate(match(x, modal)))]
+}
+
+descrever_coluna <- function(x) {
+  data.frame(
+    Média = mean(x),
+    Moda = moda(x),
+    Mediana = median(x),
+    "Variância" = var(x),
+    "Desvio-Padrão" = sd(x)
+  )
+}
+
+#Preparando o Dataset
+tabela <- read_csv("BRAZIL_CITIES.csv")
+dados <- tabela[, c("IDHM", "TAXES", "IBGE_CROP_PRODUCTION_$", "AREA", "RURAL_URBAN")]
+dados_numericos <- tabela[, c("IDHM", "TAXES", "IBGE_CROP_PRODUCTION_$", "AREA")] # removendo rural_urban por ser qualitativa
+
+# Dados antes de tratar
+pie(sort(table(dados$RURAL_URBAN)))
+
+# Tratar dados
+# Remover os valores 0 e os valores menores que 0
+
+dados_negativos_ou_na <- (dados_numericos <= 0) | (is.na(dados_numericos))
+length(which(dados_negativos_ou_na))
+dados <- dados[!apply(dados_negativos_ou_na, 1, any), ]
+
+# Contar quantas linhas sobraram após a remoção
+tamanho_dataset_apos_remover_negativos <- nrow(dados)
+
+# Exibir o número de linhas restantes
+cat("Número de linhas restantes:", tamanho_dataset_apos_remover_negativos, "\n")
+
+boxplot(dados$IDHM)
+boxplot(dados$TAXES)
+boxplot(dados[["IBGE_CROP_PRODUCTION_$"]])
+boxplot(dados$AREA)
+
+
+### Tratamento dos dados ###
+# transformação logarítmica em TAXES
+dados$TAXES <- log(dados$TAXES)
+
+# transformação logarítmica em IBGE_CROP_PRODUCTION
+dados[["IBGE_CROP_PRODUCTION_$"]] <- log(dados[["IBGE_CROP_PRODUCTION_$"]])
+
+# transformação logarítmica em AREA
+dados$AREA <- log(dados$AREA)
+
+datatable(sapply(dplyr::select(dados, -RURAL_URBAN), descrever_coluna))
+
+# Variáveis Quantitativas
+boxplot(dados$IDHM)
+boxplot(dados$TAXES)
+boxplot(dados[["IBGE_CROP_PRODUCTION_$"]])
+boxplot(dados$AREA)
+
+# Variáveis Qualitativas
+pie(sort(table(dados$RURAL_URBAN)))
+sort(table(dados$RURAL_URBAN))
+
+
+
+# Transformar qualitativas em DUMMY
+# Fixa-se Intermediário Adjacente.
+dados <- dummy_cols(dados, select_columns = "RURAL_URBAN", remove_first_dummy = TRUE)
+dados <- dplyr::select(dados, -RURAL_URBAN)
